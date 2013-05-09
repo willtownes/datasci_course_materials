86c4243f538fe3351850057cf5d1026ca446443b
Will Townes <will.townes@gmail.com>
2013-05-09 12:08:18 -0400

    modified print program to print the text of the tweets instead of the whole tweet object

--- assignment1/print.py
+++ assignment1/print.py
@@ -4,15 +4,17 @@ Prints out first ten pages of tweets about a particular keyword.
 import urllib,json
 
 def getTweets(term,pages=10):
-    '''fetch and yield (in json) tweets pertaining to the search term'''
+    '''fetch and yield unicode text of tweets pertaining to the search term'''
     for page in xrange(1,pages+1):
         response = urllib.urlopen("http://search.twitter.com/search.json?q=%s&page=%d"%(term,page))
-        yield json.load(response)
+        results = json.load(response)["results"]
+        for tweet in results:
+            yield tweet["text"]
         
 def main():
     '''print out the tweets'''
     for j in getTweets("microsoft"):
-        print(j)
+        print(j.encode("utf-8"))
         
 if __name__ == "__main__":
     main()
