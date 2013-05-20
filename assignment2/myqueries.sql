ab9d380b48e7d5b942ab7ec7dc3877a8792d6391
Will Townes <will.townes@gmail.com>
2013-05-20 19:59:38 -0400

    completed assignment2

--- assignment2/myqueries.sql
+++ assignment2/myqueries.sql
@@ -21,7 +21,32 @@ select distinct docid from frequency f1 where term='transactions'
 INTERSECT
 select distinct docid from frequency f2 where term='world'
 );
---problem 2
+--problem 2g
 select * from (
 select A.row_num,B.col_num,sum(A.value*B.value) from A,B where A.col_num = B.row_num group by A.row_num,B.col_num
 ) x where x.row_num = 2 and x.col_num = 3;
+--problem 3h (similarity matrix)
+select a.docid as q,b.docid as r,sum(a.count*b.count) 
+from frequency a,frequency b
+where a.term = b.term
+and a.docid < b.docid
+and a.docid='10080_txt_crude'
+and b.docid='17035_txt_earn'
+group by a.docid,b.docid;
+--problem 3i (keyword search)
+create view searcher as
+SELECT 'q' as docid, 'washington' as term, 1 as count 
+UNION
+SELECT 'q' as docid, 'taxes' as term, 1 as count
+UNION 
+SELECT 'q' as docid, 'treasury' as term, 1 as count;
+
+select a.docid,sum(a.count*b.count) as similarity 
+from 
+(SELECT * FROM frequency where term in ('washington','taxes','treasury')) a,
+searcher b
+where a.term = b.term
+and a.docid < b.docid
+group by a.docid,b.docid
+order by similarity desc
+limit 10;
